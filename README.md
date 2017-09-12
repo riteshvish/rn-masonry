60814795d5b7fb746d60937b1fff10671b13d56b
Ritesh Vishwakarma <Ritesh@cgsadmins-iMac-2.local>
2017-09-12 20:11:49 +0530

    mas

---
+++ README.md
@@ -0,0 +1,121 @@
+## rn-masonry
+
+## Installation
+```
+npm install rn-masonry --save
+```
+
+
+A easy to use react-native component to render a masonry layout for local and remote images with support for dynamic column rendering, Custom view inside masonry.
+
+
+
+
+
+## Preview
+
+![App preview](/ios.png)
+![App preview](/android.png)
+
+
+
+## Props
+
+### Masonry props
+
+| Props    | type   | description                                                                                             | required | default                          |
+|:----------|:--------|:---------------------------------------------------------------------------------------------------------|:----------------------------------|:------------|
+| data    | array | array of data for the data to be rendered | required |[]
+| renderView    | func(props) | function to render the card based on the View| required |
+| cols    | number | number of cols want to show | required | 2
+| gridPadding    | number | padding inside card | | 4
+| gridMargin    | number | space between two card	 | | 4
+| onScroll    | func | onScroll Event	 | |
+
+### MasonryImage props
+
+| Props    | type   | description                                                                                             | required | default                          |
+|:----------|:--------|:---------------------------------------------------------------------------------------------------------|:----------------------------------|:------------|
+| source    |object | source object like image | required |
+| resizeMode    | string| resizeMode object like image | | cover
+| style    |object|  | |
+
+source will only work on object  **(required('../img.jpg') will not work)**
+
+
+
+
+
+
+
+## Usage example
+
+
+```javascript
+import {
+MasonryImage,Masonry
+} from 'rn-masonry';
+
+
+
+
+
+class CustomView extends Component {
+
+  render() {
+
+    return (
+      <View style={[this.props.style,{backgroundColor:'#ccc'
+      ,borderRadius:5}]}>
+        <MasonryImage
+          resizeMode={"cover"}
+          gridWidth={this.props.gridWidth}
+          style={{borderRadius:5}}
+          source={{uri:this.props.data.uri}}>
+        </MasonryImage>
+        <Text>{this.props.data.title}</Text>
+      </View>
+
+    )
+  }
+}
+
+class MasonryExample extends Component {
+  renderView(props) {
+    return (
+      <CustomView
+        {...props}
+      />
+    );
+  }
+  render() {
+    SplashScreen.hide();
+    let data=[]
+    var image=[
+        'http://image1.jpg',
+        'http://image2.jpg',
+        'http://image3.jpg'
+    ]
+    for (var i = 0; i < 50; i++) {
+      data.push({"title":"Item "+i,uri:image[parseInt(Math.random()*5)%3]})
+    }
+    return (
+      <View style={{flex:1}}>
+
+        <View style={{flex:1}}>
+          <Masonry
+            cols={3}
+            renderView={this.renderView}
+            gridPadding={5}
+            gridMargin={5}
+            data={data}
+            ></Masonry>
+        </View>
+      </View>
+
+    )
+  }
+}
+
+export default MasonryExample ;
+```
--- index.js
+++ index.js
@@ -1,1 +1,136 @@
- console.log("Ritesh vishwakarma");
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Platform ,
+  Dimensions,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+export class Masonry extends Component {
+  createRows(rows,cols,odd,gridWidth,i){
+  var rowsR=[]
+  let gridPadding=this.props.gridPadding;
+  let gridMargin=this.props.gridMargin;
+
+
+  var eRow=(odd>0)?1:0;
+    for (var j = 0; j < cols+eRow; j++) {
+      rowsR.push(
+            <View key={Math.random()}>
+                {this.props.renderView(
+                    {
+                      data:(this.props.data[(i+(j*rows))]),
+                      style:{padding:gridPadding,margin:gridMargin},
+                      gridWidth:gridWidth
+                    }
+                  )
+               }
+            </View>
+          )
+
+      }
+    return rowsR;
+  }
+
+  createCols(width,cols){
+    var rowsR=[];
+    let gridPadding=this.props.gridPadding;
+    let gridMargin=this.props.gridMargin;
+    let gWidth=width/cols;
+    gWidth=gWidth-(gridPadding+gridMargin)*2
+    let odd=this.props.data.length%cols;
+    let rows=(this.props.data.length-odd)/cols;
+
+    for (var i = 0; i < cols; i++) {
+      rowsR.push(<View style={{flex:1}} key={Math.random()}>{this.createRows(cols,rows,odd--,gWidth,i)}</View>)
+    }
+    return(
+      rowsR
+    )
+  }
+  render() {
+    let props=this.props;
+    let cols=props.cols;
+    var width = Dimensions.get('window').width;
+    let gridPadding=this.props.gridPadding;
+    let gridMargin=this.props.gridMargin;
+    return (
+      <ScrollView onScroll={this.props.onScroll} >
+        <View style={{flexDirection:'row',flex:1,marginLeft:gridMargin/2,marginRight:gridMargin/2}}>
+          {this.createCols(width,cols)}
+        </View>
+      </ScrollView>
+
+    )
+  }
+}
+Masonry.defaultProps = {
+  onScroll: () => {},
+  cols: 2,
+  gridPadding: 4,
+  gridMargin: 4,
+  data:[]
+};
+
+Masonry.propTypes = {
+  onScroll: React.PropTypes.func,
+  cols: React.PropTypes.number,
+  gridPadding: React.PropTypes.number,
+  gridMargin: React.PropTypes.number
+};
+
+export class MasonryImage extends Component {
+  constructor(props){
+    super(props)
+  }
+  findPoint(a,b,c){
+    return (a*c/b);
+  }
+  state={
+    height:this.props.gridWidth
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return this.state.height!==nextState.height;
+ }
+  getImage(){
+    Image.getSize(this.props.source.uri, (width, height) => {
+      let pImageHeight=this.findPoint(this.props.gridWidth,width,height);
+        this.setState({
+          height:pImageHeight
+        })
+    })
+    return (<Image
+      source={this.props.source}
+      resizeMode={this.props.resizeMode}
+      style={[this.props.style,{height:this.state.height,
+        backgroundColor:'transparent'}]}
+      >
+      {this.props.children}
+    </Image>)
+
+  }
+  render() {
+    return (
+      <View>
+        {this.getImage()}
+      </View>
+    )
+  }
+}
+
+MasonryImage.defaultProps = {
+  gridWidth: 200,
+  source: {},
+  resizeMode: "cover",
+  style:{}
+};
+
+MasonryImage.propTypes = {
+  gridWidth: React.PropTypes.number,
+  source: React.PropTypes.object,
+  resizeMode: React.PropTypes.string,
+  style: React.PropTypes.object
+};
